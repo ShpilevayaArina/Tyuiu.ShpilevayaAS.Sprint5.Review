264bbebbe969653015db11fa7846c75bef98e21c
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-11-28 14:48:37 +0500

    Add project files.

---
+++ Tyuiu.ShpilevayaAS.Sprint5.TaskReview.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShpilevayaAS.Sprint5.TaskReview.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShpilevayaAS.Sprint5.TaskReview.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd939586-c15b-48e7-a159-665c91a8d9a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
